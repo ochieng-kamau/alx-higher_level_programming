a353fc0d9921173bbbcceda572cee585ac5fabe4
Ochieng Kamau <ochiengkamauj@gmail.com>
2022-08-24 14:02:40 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#Print the alphabet in reverse order alternating upper and lower cases
+# Print the alphabet in reverse order alternating upper and lower cases
 
 x = 0
 for c in range(ord('z'), ord('a') - 1, -1):
