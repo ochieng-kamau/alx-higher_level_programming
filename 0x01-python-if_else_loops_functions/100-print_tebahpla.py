08f732d1b7aade38f5af2ae8b6ebadf2f6809071
Ochieng Kamau <ochiengkamauj@gmail.com>
2022-08-24 10:13:51 +0000

    Added script that prints alphebet in reverse and alternates the cases

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+#Print the alphabet in reverse order alternating upper and lower cases
+
+x = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - x)), end="")
+    x = 32 if x == 0 else 0
