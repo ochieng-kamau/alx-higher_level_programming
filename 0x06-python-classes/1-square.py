a596ac5d3cb7b66b9c7418aeb78141b20ec025bf
Ochieng Kamau <ochiengkamauj@gmail.com>
2022-09-14 16:55:12 +0000

    Added 1-square.py file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square
+        Args: size
+        """
+        self.__size = size
